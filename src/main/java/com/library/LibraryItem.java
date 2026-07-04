40c6f2e7968548523813f3b4e59124a79b7592e3
Ntsako <Ngobeni2302@gmail.com>
2026-07-04 23:01:35 +0200

    Methods creation in LibraryItem.java

--- src/main/java/com/library/LibraryItem.java
+++ src/main/java/com/library/LibraryItem.java
@@ -15,7 +15,28 @@ public class LibraryItem {
     public String getId(){ return this.id;}
     public boolean isAvailable(){ return isAvailable;}
 
+    public void checkOut(){
+        if (isAvailable == true){
+            isAvailable = false;
+            System.out.println(title + " has been successfully checked out.");
+        }else {
+            System.out.println( title + " is already checked out!");
+        }
+    }
 
+    public void returnItem(){
+        if (isAvailable == false){
+            isAvailable = true;
+            System.out.println(title + " has been returned.");
+        }
+    }
+
+    public void displayDetails() {
+        System.out.println("--- Library Item Details ---");
+        System.out.println("Title: " + this.title);
+        System.out.println("Item ID: " + this.id);
+        System.out.println("Status: " + (isAvailable ? "Available" : "Checked Out"));
+    }
 
 
 }
