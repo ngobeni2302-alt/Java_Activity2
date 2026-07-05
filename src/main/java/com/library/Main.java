bdb74eadafa79647cd73b8cd016914165ab53e53
Ntsako <Ngobeni2302@gmail.com>
2026-07-05 19:25:50 +0200

    Main.java

--- src/main/java/com/library/Main.java
+++ src/main/java/com/library/Main.java
@@ -1,5 +1,34 @@
 package com.library;
 
 public class Main {
+    public static void main(String[] args){
+        Book theLongWalk = new Book("The Hobbt","B101", "J.R.R Tolkien");
+        Magazine animalGeo = new Magazine("National Geographic", "M202", 145);
+
+        System.out.println(theLongWalk);
+        System.out.println(animalGeo);
+
+        System.out.println();
+
+        theLongWalk.displayDetails();
+        animalGeo.displayDetails();
+
+        System.out.println();
+
+        theLongWalk.checkOut();
+        animalGeo.checkOut();
+
+        System.out.println();
+
+        theLongWalk.checkOut();
+        animalGeo.checkOut();
+
+        System.out.println();
+
+        theLongWalk.returnItem();
+        animalGeo.returnItem();
+    }
+
+
 
 }
