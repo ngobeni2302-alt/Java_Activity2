9796af541ba4cab21cd234301b160dc193a7401b
Ntsako <Ngobeni2302@gmail.com>
2026-07-05 18:01:24 +0200

    Book.java completion

--- src/main/java/com/library/Book.java
+++ src/main/java/com/library/Book.java
@@ -1,5 +1,22 @@
 package com.library;
 
 public class Book extends LibraryItem {
+    private String author;
+
+    public Book(String title, String id, String author){
+        super(title, id);
+        this.author = author;
+    }
+
+    public String getAuthor(){ return this.author;}
+
+    @Override
+    public void displayDetails(){
+        System.out.println("--- Library Item Details ---");
+        System.out.println("Title: " + getTitle());
+        System.out.println("Item ID: " + getId());
+        System.out.println("Author: " + this.author);
+        System.out.println("Status: " + (isAvailable() ? "Available" : "Checked Out"));
+    }
 
 }
