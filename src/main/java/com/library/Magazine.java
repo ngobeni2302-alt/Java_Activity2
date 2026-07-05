19373c049cb884b5f18be412dfc6e7b7f17fa1e9
Ntsako <Ngobeni2302@gmail.com>
2026-07-05 18:10:33 +0200

    Magazine.java completion

--- src/main/java/com/library/Magazine.java
+++ src/main/java/com/library/Magazine.java
@@ -1,5 +1,21 @@
 package com.library;
 
 public class Magazine extends LibraryItem {
+    private int issueNumber;
+
+    public Magazine(String title, String id, int issueNumber){
+        super(title, id);
+        this.issueNumber = issueNumber;
+    }
+
+    public int getIssueNumber(){ return this.issueNumber;}
+
+    @Override
+
+    public void displayDetails(){
+        super.displayDetails();
+        System.out.println("IssueNumber: " + this.issueNumber);
+    }
+
 
 }
